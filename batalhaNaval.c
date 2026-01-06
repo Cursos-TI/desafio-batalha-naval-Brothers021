7766eda37e5e87d21b80db4b21af3f49ef7fe924
Matheus M. <dallalokomatheus07@gmail.com>
2026-01-06 18:51:34 +0000

    impressões do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -25,5 +25,20 @@ int main() {
     for (int i = 5; i < 8; i++) {
         tabuleiro[i][7] = 3;
     }
+// aparecer cabeçalho da coluna
+    printf(" 0 1 2 3 4 5 6 7 8 9\n");
+    for (int j = 0; j < 10; j++){
+        printf("%2d ", j);
+    }
+    printf("\n");
+
+
+// aparecer o tabuleiro
+   for (int i = 0; i < 10; i++){
+    printf("%c ", linha[i]);
+   for (int j = 0; j < 10; j++)
+   printf("%2d ", tabuleiro[i][j]);
+}
+   printf("\n");
     return 0;
 }
